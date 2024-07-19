1057342a1a806393001566d1b80a5b4976e65b1b
anjalidevaraju <anjalidevaraju996@gmail.com>
2024-07-19 17:08:43 +0530

    Entry Form Table Update

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import EntryForm from './EntryForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Entry Form</h1>
+      </header>
+      <EntryForm />
+    </div>
+  );
+}
+
+export default App;
